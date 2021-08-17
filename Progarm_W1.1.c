6441c3dcbb8fc007aa7dca53bfd66601997177a2
Chanakarnsua <89014026+Chanakarnsua@users.noreply.github.com>
2021-08-17 16:44:45 +0700

    [64010146]_progarm1
    จงเขียนโปรแกรมเพื่อรับค่าตัวเลข 2 ตัว แล้วแสดงผลบวก ลบ คูณ หาร ของตัวเลขทั้งสองตัวนั้น โดยใช้ตัวแปรน้อยที่สุด (Level 1)

---
+++ Progarm_W1.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int a;
+int b;
+int main()
+{
+printf("Enter a,b : ");
+scanf("%d,%d", &a, &b);
+printf("a + b =  %d\n", a+b);
+printf("a - b =  %d\n", a-b);
+printf("a * b =  %d\n", a*b);
+printf("a / b =  %d\n", a/b);
+    return 0;
+}
---
+++ Progarm_W1.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int a;
+int b;
+int main()
+{
+printf("Enter a,b : ");
+scanf("%d,%d", &a, &b);
+int plus1 = a+b;
+int plus2 = a-b;
+int plus3 = a*b;
+int plus4 = a/b;
+printf(" a + b =  %d\n a - b =  %d\n a * b =  %d\n a / b =  %d\n", plus1, plus2, plus3, plus4);
+    return 0;
+}
